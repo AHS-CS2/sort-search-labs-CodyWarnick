a3da61ecaa4223bc70b14c14060930cdc8a7a9db
CODY WARNICK <226010@andrews.esc18.net>
2026-03-30 11:37:06 -0500

    Word Sort lab finished

--- src/main/java/WordSortRunner.java
+++ src/main/java/WordSortRunner.java
@@ -1,17 +1,17 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Cody Warnick
 
 import java.util.Arrays;
-import static java.lang.System.*; 
+import static java.lang.System.*;
 
 public class WordSortRunner
 {
 	public static void main(String args[])
 	{
 		WordSort ws = new WordSort("abc ABC 12321 fred alexander");
-		String[] array = ws.sort();
-		System.out.println(array);
+		ws.sort();
+		System.out.println(ws.toString());
 		
 		//...
 		
